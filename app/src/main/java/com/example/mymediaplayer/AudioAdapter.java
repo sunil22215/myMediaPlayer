cb6123c6497473d06f0e636ee986030655afd957
Sagar Shinde <sagar.shinde@docterz.in>
2020-12-10 21:02:59 +0530

    Code format and changed views id name

--- app/src/main/java/com/example/mymediaplayer/AudioAdapter.java
+++ app/src/main/java/com/example/mymediaplayer/AudioAdapter.java
@@ -8,40 +8,43 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class AudioAdapter extends RecyclerView.Adapter<AudioAdapter.ViewHolder> {
 
-    private String[] items;
+    private final ArrayList<File> items;
 
-    public AudioAdapter(String[] items){
-        this.items=items;
+    public AudioAdapter(ArrayList<File> items) {
+        this.items = items;
     }
+
     @NonNull
     @Override
     public AudioAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater=LayoutInflater.from(parent.getContext());
-        View listItems=layoutInflater.inflate(R.layout.audio_list,parent,false);
-        ViewHolder viewHolder=new ViewHolder(listItems);
-        return viewHolder;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItems = layoutInflater.inflate(R.layout.row_audio_list, parent, false);
+        return new ViewHolder(listItems);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        final  String string= items[position];
-        holder.audioName.setText(items[position]);
-
-
+        final String string = items.get(position).toString();
+        holder.textViewAudioName.setText(string);
     }
 
     @Override
     public int getItemCount() {
-        return items.length;
+        return items.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView audioName;
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textViewAudioName;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.audioName=(TextView) itemView.findViewById(R.id.audioName);
+            textViewAudioName = itemView.findViewById(R.id.textViewAudioName);
         }
     }
 }
--- app/src/main/java/com/example/mymediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mymediaplayer/MainActivity.java
@@ -25,50 +25,44 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-   // ListView listView;
-    RecyclerView audioListView;
-    String[] items;
+
+    private RecyclerView recyclerViewAudioList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        audioListView = (RecyclerView) findViewById(R.id.audioListView);
+        initLayoutView();
+
+        fetchAudioList();
 
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
             runTimePermission();
         }
     }
 
-    public void runTimePermission() {
-        Dexter.withActivity(this)
-                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                .withListener(new PermissionListener() {
-                    @Override
-                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        display();
-
-                    }
-
-                    @Override
-                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-
-                    }
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                        permissionToken.continuePermissionRequest();
-                    }
-                }).check();
+    private void fetchAudioList() {
+        final ArrayList<File> audioFileList = findAllAudioFiles(Environment.getExternalStorageDirectory());
+        AudioAdapter adapter = new AudioAdapter(audioFileList);
+        recyclerViewAudioList.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewAudioList.setAdapter(adapter);
     }
 
-    public ArrayList<File> findSong(File file) {
+    private void initLayoutView() {
+        recyclerViewAudioList = findViewById(R.id.recyclerViewAudioList);
+    }
+
+    private void runTimePermission() {
+
+    }
+
+    private ArrayList<File> findAllAudioFiles(File file) {
         ArrayList<File> arrayList = new ArrayList<>();
         File[] files = file.listFiles();
         for (File singleFile : files) {
             if (singleFile.isDirectory() && !singleFile.isHidden()) {
-                arrayList.addAll(findSong(singleFile));
+                arrayList.addAll(findAllAudioFiles(singleFile));
             } else {
                 if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
                     arrayList.add(singleFile);
@@ -77,39 +71,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return arrayList;
     }
-
-   void display() {
-       final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
-       items = new String[mySongs.size()];
-       for (int i = 0; i < mySongs.size(); i++) {
-           items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
-
-       }
-
-       audioListView=(RecyclerView) findViewById(R.id.audioListView);
-       AudioAdapter adapter=new AudioAdapter(items);
-       audioListView.setLayoutManager(new LinearLayoutManager(this));
-       audioListView.setAdapter(adapter);
-   }
-       /* ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-        recyclerView.setAdapter(myAdapter);
-
-        recyclerView.setOnClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String songName = recyclerView.getChildItemId(position).toString();
-
-                Intent intent = new Intent(MainActivity.this, PlayerActivity.class);
-                intent.putExtra("songs", mySongs);
-                intent.putExtra("songame", songName);
-                intent.putExtra("pos", position);
-                startActivity(intent);
-
-                startActivity(new Intent(getApplicationContext(), PlayerActivity.class)
-                        .putExtra("songs", mySongs).putExtra("songame", songName)
-                        .putExtra("pos", position));
-
-            }
-        });
-    */
 }
